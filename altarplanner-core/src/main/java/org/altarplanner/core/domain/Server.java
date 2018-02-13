13081c2da04ec842b042bbbbd7de557bf7455098
Martin Kröning <m.kroening@hotmail.de>
2018-02-13 21:34:27 +0100

    Add weekly absences to server

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Server.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Server.java
@@ -3,6 +3,7 @@ package org.altarplanner.core.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ public class Server {
     @Getter @Setter private String surname;
     @Getter @Setter private int year = LocalDate.now().getYear();
     @Getter @Setter private List<DateSpan> absences = new ArrayList<>();
+    @Getter @Setter private List<DayOfWeek> weeklyAbsences = new ArrayList<>();
 
     public Server() {
         this.surname = Config.RESOURCE_BUNDLE.getString("server.surname");

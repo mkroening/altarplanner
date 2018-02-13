507b324f5c976c5c7b7fc70ff16b6661f3610e06
Martin Kröning <m.kroening@hotmail.de>
2018-02-13 21:14:33 +0100

    Create config class with static resource bundle

---
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Config.java
@@ -0,0 +1,9 @@
+package org.altarplanner.core.domain;
+
+import java.util.ResourceBundle;
+
+public class Config {
+
+    public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("org.altarplanner.core.locale.locale");
+
+}

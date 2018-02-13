c7c05c50576593fd7ffd18ac6ff293b6f261b68b
Martin Kröning <m.kroening@hotmail.de>
2018-02-13 21:38:34 +0100

    Add dateTimeOnWishes to server

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Server.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Server.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class Server {
     @Getter @Setter private int year = LocalDate.now().getYear();
     @Getter @Setter private List<DateSpan> absences = new ArrayList<>();
     @Getter @Setter private List<DayOfWeek> weeklyAbsences = new ArrayList<>();
+    @Getter @Setter private List<LocalDateTime> dateTimeOnWishes = new ArrayList<>();
 
     public Server() {
         this.surname = Config.RESOURCE_BUNDLE.getString("server.surname");

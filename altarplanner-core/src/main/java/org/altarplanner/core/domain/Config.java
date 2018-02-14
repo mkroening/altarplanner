bc7aeb77e69bc41eabb3ca799ced089d3d7f69c6
Martin Kröning <m.kroening@hotmail.de>
2018-02-14 16:24:31 +0100

    Add serviceTypes, regularMasses and servers field to Config

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Config.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Config.java
@@ -1,9 +1,19 @@
 package org.altarplanner.core.domain;
 
-import java.util.ResourceBundle;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.altarplanner.core.domain.mass.RegularMass;
 
+import java.util.*;
+
+@NoArgsConstructor
 public class Config {
 
     public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("org.altarplanner.core.locale.locale");
+
+    @Getter @Setter private List<ServiceType> serviceTypes = new ArrayList<>();
+    @Getter @Setter private List<RegularMass> regularMasses = new ArrayList<>();
+    @Getter @Setter private List<Server> servers = new ArrayList<>();
 
 }

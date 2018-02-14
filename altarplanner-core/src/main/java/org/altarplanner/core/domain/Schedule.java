1e5f48863bd3b2168f3c73b441e61e595717fefb
Martin Kröning <m.kroening@hotmail.de>
2018-02-14 14:28:17 +0100

    Add ServiceCollector as PlanningEntityCollectionProperty

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Schedule.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Schedule.java
@@ -3,6 +3,7 @@ package org.altarplanner.core.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.altarplanner.core.domain.mass.PlanningMass;
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
 import org.optaplanner.core.api.domain.solution.PlanningScore;
 import org.optaplanner.core.api.domain.solution.PlanningSolution;
 import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
@@ -10,6 +11,7 @@ import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @PlanningSolution
 public class Schedule {
@@ -24,6 +26,13 @@ public class Schedule {
     public Schedule() {
         this.servers = new ArrayList<>();
         this.masses = new ArrayList<>();
+    }
+
+    @PlanningEntityCollectionProperty
+    public List<Service> getServices() {
+        return masses.parallelStream()
+                .flatMap(mass -> mass.getServices().parallelStream())
+                .collect(Collectors.toList());
     }
 
 }

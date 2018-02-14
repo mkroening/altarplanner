29ae3c72974ddbb660d1ff16c607580d92a9ab21
Martin Kröning <m.kroening@hotmail.de>
2018-02-14 14:23:53 +0100

    Make Service the planning entity, making its server field the planning variable

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Service.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Service.java
@@ -3,12 +3,16 @@ package org.altarplanner.core.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.altarplanner.core.domain.mass.PlanningMass;
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.variable.PlanningVariable;
 
+@PlanningEntity
 public class Service {
 
     @Getter @Setter private int id;
     @Getter private final PlanningMass mass;
     @Getter private final ServiceType type;
+    @PlanningVariable(valueRangeProviderRefs = {"serverRange"})
     @Getter @Setter private Server server;
 
     public Service(PlanningMass mass, ServiceType type) {

51cd0b7bc47ca09f33f3924f0f8e28388cee3356
Martin Kröning <m.kroening@hotmail.de>
2018-02-14 16:25:35 +0100

    Add DiscreteMass accumulator from regularMasses to Config

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Config.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Config.java
@@ -3,9 +3,13 @@ package org.altarplanner.core.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.altarplanner.core.domain.mass.DiscreteMass;
 import org.altarplanner.core.domain.mass.RegularMass;
 
+import java.time.DayOfWeek;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @NoArgsConstructor
 public class Config {
@@ -15,5 +19,14 @@ public class Config {
     @Getter @Setter private List<ServiceType> serviceTypes = new ArrayList<>();
     @Getter @Setter private List<RegularMass> regularMasses = new ArrayList<>();
     @Getter @Setter private List<Server> servers = new ArrayList<>();
+
+    public Stream<DiscreteMass> getDiscreteMassParallelStreamWithin(DateSpan dateSpan) {
+        Map<DayOfWeek, List<RegularMass>> dayMassMap = regularMasses.parallelStream()
+                .collect(Collectors.groupingBy(RegularMass::getDay));
+
+        return dateSpan.getDateParallelStream()
+                .flatMap(date -> dayMassMap.get(date.getDayOfWeek()).parallelStream()
+                        .map(regularMass -> new DiscreteMass(regularMass, date)));
+    }
 
 }

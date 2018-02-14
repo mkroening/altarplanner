2407fcfd34d3202cc5be5cb4fa41fbca8d403e3b
Martin Kröning <m.kroening@hotmail.de>
2018-02-14 14:24:56 +0100

    Make Schedule the planning solution, making its servers field the Server valueRangeProvider

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Schedule.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Schedule.java
@@ -3,13 +3,17 @@ package org.altarplanner.core.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.altarplanner.core.domain.mass.PlanningMass;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@PlanningSolution
 public class Schedule {
 
     @Getter @Setter private DateSpan planningWindow;
+    @ValueRangeProvider(id = "serverRange")
     @Getter private final List<Server> servers;
     @Getter private final List<PlanningMass> masses;
 

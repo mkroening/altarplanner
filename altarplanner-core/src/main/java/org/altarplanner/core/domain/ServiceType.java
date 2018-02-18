4ddcf9aa6dd265d9f21718593a626e6896fa2afc
Martin Kröning <m.kroening@hotmail.de>
2018-02-18 15:10:05 +0100

    Fix ServiceType year fields encapsulation

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/ServiceType.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/ServiceType.java
@@ -8,8 +8,8 @@ import java.time.Year;
 public class ServiceType {
 
     @Getter @Setter private String name;
-    @Getter @Setter int maxYear = Year.now().getValue();
-    @Getter @Setter int minYear = Year.now().getValue() - 6;
+    @Getter @Setter private int maxYear = Year.now().getValue();
+    @Getter @Setter private int minYear = Year.now().getValue() - 6;
 
     public ServiceType() {
         this.name = Config.RESOURCE_BUNDLE.getString("serviceType.name");

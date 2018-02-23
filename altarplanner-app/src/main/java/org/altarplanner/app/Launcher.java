dae0975ece5e80ee9d5fdbc2217d4489546fda7c
Martin Kröning <m.kroening@hotmail.de>
2018-02-23 23:22:50 +0100

    Load config.xml on start

--- altarplanner-app/src/main/java/org/altarplanner/app/Launcher.java
+++ altarplanner-app/src/main/java/org/altarplanner/app/Launcher.java
@@ -1,16 +1,22 @@
 package org.altarplanner.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.altarplanner.core.domain.Config;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ResourceBundle;
 
 public class Launcher extends Application {
 
     public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("org.altarplanner.app.locale.locale");
+
+    private Config config;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -18,6 +24,16 @@ public class Launcher extends Application {
         primaryStage.setTitle("AltarPlanner");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    @FXML
+    public void initialize() {
+        try {
+            this.config = Config.load(new File("config.xml"));
+        } catch (FileNotFoundException e) {
+            System.out.println("File");
+            this.config = new Config();
+        }
     }
 
 }

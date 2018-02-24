80abd4ccddb110cd2e63ec48e5241b12f22ecb96
Martin Kröning <m.kroening@hotmail.de>
2018-02-24 17:39:08 +0100

    Add helper functions to ServiceType to use by the gui

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/ServiceType.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/ServiceType.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Year;
+import java.util.Comparator;
 
 public class ServiceType {
 
@@ -13,6 +14,19 @@ public class ServiceType {
 
     public ServiceType() {
         this.name = Config.RESOURCE_BUNDLE.getString("serviceType.name");
+    }
+
+    public String getDesc() {
+        return name +
+                " (" + Config.RESOURCE_BUNDLE.getString("serviceType.year") + ": " +
+                maxYear + " - " + minYear + ")";
+    }
+
+    public static Comparator<ServiceType> getAlphabeticComparator() {
+        return Comparator
+                .comparing(ServiceType::getName)
+                .thenComparing(ServiceType::getMaxYear)
+                .thenComparing(ServiceType::getMinYear);
     }
 
 }

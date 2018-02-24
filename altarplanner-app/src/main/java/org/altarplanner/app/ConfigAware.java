6f46649ad837b57aa9a83338b6947ade1954b181
Martin Kröning <m.kroening@hotmail.de>
2018-02-24 12:51:52 +0100

    Implement FXML loading Create ConfigAware interface Make Launcher implement ConfigAware Implement static loadParent function to load an FXML file into the window

---
+++ altarplanner-app/src/main/java/org/altarplanner/app/ConfigAware.java
@@ -0,0 +1,9 @@
+package org.altarplanner.app;
+
+import org.altarplanner.core.domain.Config;
+
+public interface ConfigAware {
+
+    void initConfig(Config config);
+
+}
--- altarplanner-app/src/main/java/org/altarplanner/app/Launcher.java
+++ altarplanner-app/src/main/java/org/altarplanner/app/Launcher.java
@@ -8,16 +8,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.altarplanner.core.domain.Config;
 
+import java.io.IOException;
 import java.util.ResourceBundle;
 
-public class Launcher extends Application {
+public class Launcher extends Application implements ConfigAware {
 
     public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle("org.altarplanner.app.locale.locale");
+
+    private static Stage primaryStage;
+    public static void loadParent(String location, Config config) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Launcher.class.getResource(location), RESOURCE_BUNDLE);
+        Parent root = loader.load();
+        Object controller = loader.getController();
+
+        if (controller instanceof ConfigAware)
+            ((ConfigAware) controller).initConfig(config);
+
+        String name = controller.getClass().getSimpleName();
+        String key = name.substring(0, 1).toLowerCase() + name.substring(1);
+        String title = RESOURCE_BUNDLE.getString(key);
+
+        primaryStage.setTitle("AltarPlanner - " + title);
+        primaryStage.getScene().setRoot(root);
+        primaryStage.setMinHeight(root.minHeight(-1));
+        primaryStage.setMinWidth(root.minWidth(-1));
+        primaryStage.setHeight(root.prefHeight(-1));
+        primaryStage.setWidth(root.prefWidth(-1));
+    }
 
     private Config config;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Launcher.primaryStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("launcher.fxml"), RESOURCE_BUNDLE);
         primaryStage.setTitle("AltarPlanner");
         primaryStage.setScene(new Scene(root));
@@ -27,6 +50,11 @@ public class Launcher extends Application {
     @FXML
     public void initialize() {
         this.config = Config.load();
+    }
+
+    @Override
+    public void initConfig(Config config) {
+        this.config = config;
     }
 
 }

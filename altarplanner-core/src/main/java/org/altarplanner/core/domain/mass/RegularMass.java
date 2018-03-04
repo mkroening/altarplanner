e5f5ce83bca724a548579e857f43fab1cd0a45bb
Martin Kröning <m.kroening@hotmail.de>
2018-03-04 16:43:27 +0100

    Add static alphabetic comparator to RegularMass

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/mass/RegularMass.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/mass/RegularMass.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import java.time.DayOfWeek;
 import java.time.format.TextStyle;
+import java.util.Comparator;
 import java.util.Locale;
 
 public class RegularMass extends EditableMass {
@@ -15,6 +16,14 @@ public class RegularMass extends EditableMass {
         return day.getDisplayName(TextStyle.FULL, Locale.getDefault()) + " - " +
                 getTime() + " - " +
                 getChurch();
+    }
+
+    public static Comparator<RegularMass> getAlphabeticComparator() {
+        return Comparator
+                .comparing(RegularMass::getDay)
+                .thenComparing(GenericMass::getTime)
+                .thenComparing(GenericMass::getChurch)
+                .thenComparing(GenericMass::getForm);
     }
 
 }

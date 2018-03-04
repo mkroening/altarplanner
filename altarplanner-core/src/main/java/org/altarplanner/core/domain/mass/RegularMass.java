32e3b71527a172bdff8273ef687e05205e3bdc7a
Martin Kröning <m.kroening@hotmail.de>
2018-03-04 16:37:43 +0100

    Add function to get description from RegularMass

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/mass/RegularMass.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/mass/RegularMass.java
@@ -4,9 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class RegularMass extends EditableMass {
 
     @Getter @Setter private DayOfWeek day = DayOfWeek.SUNDAY;
+
+    public String getDesc() {
+        return day.getDisplayName(TextStyle.FULL, Locale.getDefault()) + " - " +
+                getTime() + " - " +
+                getChurch();
+    }
 
 }

04c2aacb1764b2181d1ce116d79eca14d8f7480a
Martin Kröning <m.kroening@hotmail.de>
2018-03-26 11:53:32 +0200

    Rearrange read and write functions of XML

--- altarplanner-core/src/main/java/org/altarplanner/core/io/XML.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/io/XML.java
@@ -17,17 +17,17 @@ public class XML {
         return xStream;
     }
 
-    public static Object read(File inputFile, Class... xStreamAnnotatedClasses) throws FileNotFoundException {
-        XStream xStream = makeXStream(xStreamAnnotatedClasses);
-        Reader reader = new InputStreamReader(new FileInputStream(inputFile));
-        return xStream.fromXML(reader);
-    }
-
     public static void write(Object obj, File outputFile) throws FileNotFoundException {
         XStream xStream = makeXStream(obj.getClass());
         Optional.ofNullable(outputFile.getParentFile()).ifPresent(File::mkdirs);
         Writer writer = new OutputStreamWriter(new FileOutputStream(outputFile));
         xStream.toXML(obj, writer);
+    }
+
+    public static Object read(File inputFile, Class... xStreamAnnotatedClasses) throws FileNotFoundException {
+        XStream xStream = makeXStream(xStreamAnnotatedClasses);
+        Reader reader = new InputStreamReader(new FileInputStream(inputFile));
+        return xStream.fromXML(reader);
     }
 
 }

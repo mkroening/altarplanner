be635f8adadd4aa412bae676b12cb8b06b1fa0b5
Martin Kröning <m.kroening@hotmail.de>
2018-03-28 12:17:31 +0200

    Fix imports

--- altarplanner-core/src/main/java/org/altarplanner/core/domain/Server.java
+++ altarplanner-core/src/main/java/org/altarplanner/core/domain/Server.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Server implements Serializable {

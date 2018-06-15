08418e5e702e8bf43fc3a21beb14b30832d825bb
Martin Kröning <m.kroening@hotmail.de>
2018-06-15 14:12:25 +0200

    Remove DiscreteMassEditor.selectedDiscreteMass

--- altarplanner-app/src/main/java/org/altarplanner/app/planning/DiscreteMassEditor.java
+++ altarplanner-app/src/main/java/org/altarplanner/app/planning/DiscreteMassEditor.java
@@ -39,7 +39,6 @@ public class DiscreteMassEditor {
     @FXML private TableColumn<ServiceType, String> serviceTypeCountColumn;
 
     private Config config;
-    private DiscreteMass selectedDiscreteMass;
     private boolean applyChanges;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DiscreteMassEditor.class);
@@ -66,7 +65,6 @@ public class DiscreteMassEditor {
                 timeTextField.setText(newValue.getTime().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
                 churchTextField.setText(newValue.getChurch());
                 formTextField.setText(newValue.getForm());
-                selectedDiscreteMass = newValue;
                 serviceTypeCountTableView.refresh();
                 applyChanges = true;
             }
@@ -74,7 +72,7 @@ public class DiscreteMassEditor {
 
         datePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
             if (applyChanges) {
-                selectedDiscreteMass.setDate(newValue);
+                discreteMassListView.getSelectionModel().getSelectedItem().setDate(newValue);
                 discreteMassListView.getItems().sort(DiscreteMass.getDescComparator());
             }
         });
@@ -82,7 +80,7 @@ public class DiscreteMassEditor {
         timeTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (applyChanges) {
                 try {
-                    selectedDiscreteMass.setTime(LocalTime.parse(newValue, DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
+                    discreteMassListView.getSelectionModel().getSelectedItem().setTime(LocalTime.parse(newValue, DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
                     timeTextField.getStyleClass().remove("text-input-error");
                     discreteMassListView.getItems().sort(DiscreteMass.getDescComparator());
                 } catch (DateTimeParseException e) {
@@ -94,14 +92,14 @@ public class DiscreteMassEditor {
 
         churchTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (applyChanges) {
-                selectedDiscreteMass.setChurch(newValue);
+                discreteMassListView.getSelectionModel().getSelectedItem().setChurch(newValue);
                 discreteMassListView.getItems().sort(DiscreteMass.getDescComparator());
             }
         });
 
         formTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (applyChanges) {
-                selectedDiscreteMass.setForm(newValue);
+                discreteMassListView.getSelectionModel().getSelectedItem().setForm(newValue);
             }
         });
 
@@ -111,7 +109,7 @@ public class DiscreteMassEditor {
 
         serviceTypeCountColumn.setCellValueFactory(param -> {
             if (applyChanges)
-                return new SimpleStringProperty(String.valueOf(selectedDiscreteMass.getServiceTypeCount().getOrDefault(param.getValue(), 0)));
+                return new SimpleStringProperty(String.valueOf(discreteMassListView.getSelectionModel().getSelectedItem().getServiceTypeCount().getOrDefault(param.getValue(), 0)));
             else
                 return null;
         });
@@ -120,9 +118,9 @@ public class DiscreteMassEditor {
             if (applyChanges) {
                 String newValue = event.getNewValue();
                 if ("".equals(newValue) || "0".equals(newValue)) {
-                    selectedDiscreteMass.getServiceTypeCount().remove(event.getRowValue());
+                    discreteMassListView.getSelectionModel().getSelectedItem().getServiceTypeCount().remove(event.getRowValue());
                 } else try {
-                    selectedDiscreteMass.getServiceTypeCount().put(event.getRowValue(), Integer.parseInt(newValue));
+                    discreteMassListView.getSelectionModel().getSelectedItem().getServiceTypeCount().put(event.getRowValue(), Integer.parseInt(newValue));
                 } catch (NumberFormatException e) {
                     serviceTypeCountTableView.refresh();
                 }
@@ -166,7 +164,7 @@ public class DiscreteMassEditor {
     }
 
     @FXML private void removeDiscreteMass() {
-        discreteMassListView.getItems().remove(selectedDiscreteMass);
+        discreteMassListView.getItems().remove(discreteMassListView.getSelectionModel().getSelectedItem());
         if (discreteMassListView.getItems().isEmpty())
             setDisable(true);
     }
@@ -177,7 +175,7 @@ public class DiscreteMassEditor {
                         .initData(config, discreteMasses -> {
                             discreteMassListView.getItems().addAll(discreteMasses);
                             setDisable(false);
-                            if (selectedDiscreteMass == null)
+                            if (discreteMassListView.getSelectionModel().getSelectedItem() == null)
                                 discreteMassListView.getSelectionModel().selectFirst();
                             discreteMassListView.getItems().sort(DiscreteMass.getDescComparator());
                         }));

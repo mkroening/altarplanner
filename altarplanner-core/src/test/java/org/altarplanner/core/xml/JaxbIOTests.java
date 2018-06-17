51feea516e31bfc913171a17999bfd43ca855945
Martin Kröning <m.kroening@hotmail.de>
2018-06-17 22:52:06 +0200

    Implement tests for JaxbIO with DiscreteMassCollection

--- altarplanner-core/src/test/java/org/altarplanner/core/xml/JaxbIOTests.java
+++ altarplanner-core/src/test/java/org/altarplanner/core/xml/JaxbIOTests.java
@@ -2,6 +2,7 @@ package org.altarplanner.core.xml;
 
 import org.altarplanner.core.domain.Config;
 import org.altarplanner.core.domain.util.BigDomainGenerator;
+import org.altarplanner.core.xml.jaxb.util.DiscreteMassCollection;
 import org.junit.Test;
 
 import java.io.File;
@@ -16,7 +17,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JaxbIOTests {
 
-    private static final File EXPECTED_CONFIG = new File("src/test/resources/org/altarplanner/core/xml/bigDomainConfig.xml");
+    private static final String XML_TEST_PATHNAME = "src/test/resources/org/altarplanner/core/xml/";
+    private static final File EXPECTED_CONFIG = new File(XML_TEST_PATHNAME + "bigDomainConfig.xml");
+    private static final File EXPECTED_DISCRETE_MASS_COLLECTION = new File(XML_TEST_PATHNAME + "bigDiscreteMassCollection.xml");
 
     @Test
     public void configUnmarshalling() throws FileNotFoundException, UnexpectedElementException, UnknownJAXBException {
@@ -30,6 +33,23 @@ public class JaxbIOTests {
         final List<String> expectedLines = Files.lines(EXPECTED_CONFIG.toPath()).collect(Collectors.toUnmodifiableList());
         final Path marshalledPath = Files.createTempFile(null, null);
         JaxbIO.marshal(BigDomainGenerator.genConfig(), marshalledPath.toFile());
+        final List<String> marshalledLines = Files.lines(marshalledPath).collect(Collectors.toUnmodifiableList());
+        Files.delete(marshalledPath);
+        assertEquals(expectedLines, marshalledLines);
+    }
+
+    @Test
+    public void discreteMassCollectionUnmarschalling() throws FileNotFoundException, UnexpectedElementException, UnknownJAXBException {
+        final DiscreteMassCollection expected = BigDomainGenerator.genMasses();
+        final DiscreteMassCollection unmarshalled = JaxbIO.unmarshal(EXPECTED_DISCRETE_MASS_COLLECTION, DiscreteMassCollection.class);
+        assertEquals(expected, unmarshalled);
+    }
+
+    @Test
+    public void discreteMassCollectionMarshalling() throws IOException, UnknownJAXBException {
+        final List<String> expectedLines = Files.lines(EXPECTED_DISCRETE_MASS_COLLECTION.toPath()).collect(Collectors.toUnmodifiableList());
+        final Path marshalledPath = Files.createTempFile(null, null);
+        JaxbIO.marshal(BigDomainGenerator.genMasses(), marshalledPath.toFile());
         final List<String> marshalledLines = Files.lines(marshalledPath).collect(Collectors.toUnmodifiableList());
         Files.delete(marshalledPath);
         assertEquals(expectedLines, marshalledLines);

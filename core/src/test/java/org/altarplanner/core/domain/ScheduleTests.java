240f4b616579c40c00b957165a9aa23b3051ce07
Martin Kröning <m.kroening@hotmail.de>
2019-01-20 10:07:47 +0100

    Use last monday instead of today in ScheduleTests

--- core/src/test/java/org/altarplanner/core/domain/ScheduleTests.java
+++ core/src/test/java/org/altarplanner/core/domain/ScheduleTests.java
@@ -4,6 +4,7 @@ import org.altarplanner.core.domain.mass.PlanningMassTemplate;
 import org.altarplanner.core.domain.mass.RegularMass;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -14,17 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 
 class ScheduleTests {
 
-    private final static LocalDate TODAY = LocalDate.now();
+    private final static LocalDate LAST_MONDAY = LocalDate.now().plusDays(DayOfWeek.MONDAY.getValue() - LocalDate.now().getDayOfWeek().getValue());
 
     @Test
     void constructorPublishedRelevanceTest() {
         final var config = new Config();
         final var firstMass = new PlanningMassTemplate();
-        firstMass.setDateTime(LocalDateTime.of(TODAY.minusWeeks(2), LocalTime.of(11,0)));
+        firstMass.setDateTime(LocalDateTime.of(LAST_MONDAY.minusWeeks(2), LocalTime.of(11,0)));
         final var secondMass = new PlanningMassTemplate();
-        secondMass.setDateTime(LocalDateTime.of(TODAY, LocalTime.of(11,0)));
+        secondMass.setDateTime(LocalDateTime.of(LAST_MONDAY, LocalTime.of(11,0)));
         final var thirdMass = new PlanningMassTemplate();
-        thirdMass.setDateTime(LocalDateTime.of(TODAY.plusWeeks(2), LocalTime.of(11,0)));
+        thirdMass.setDateTime(LocalDateTime.of(LAST_MONDAY.plusWeeks(2), LocalTime.of(11,0)));
         final var publishedSchedule = new Schedule(new ScheduleTemplate(List.of(firstMass, secondMass)), config);
         final var schedule = new Schedule(new ScheduleTemplate(List.of(thirdMass)), publishedSchedule, config);
         assertEquals(1, schedule.getPublishedMasses().size());
@@ -35,9 +36,9 @@ class ScheduleTests {
     void constructorPublishedFutureTest() {
         final var config = new Config();
         final var firstMass = new PlanningMassTemplate();
-        firstMass.setDateTime(LocalDateTime.of(TODAY.minusWeeks(6), LocalTime.of(11,0)));
+        firstMass.setDateTime(LocalDateTime.of(LAST_MONDAY.minusWeeks(6), LocalTime.of(11,0)));
         final var secondMass = new PlanningMassTemplate();
-        secondMass.setDateTime(LocalDateTime.of(TODAY, LocalTime.of(11,0)));
+        secondMass.setDateTime(LocalDateTime.of(LAST_MONDAY, LocalTime.of(11,0)));
         final var publishedSchedule = new Schedule(new ScheduleTemplate(List.of(secondMass)), config);
         final var schedule = new Schedule(new ScheduleTemplate(List.of(firstMass)), publishedSchedule, config);
         assertEquals(1, schedule.getPublishedMasses().size());
@@ -48,21 +49,21 @@ class ScheduleTests {
     void constructorFutureDraftTest() {
         final var config = new Config();
         final var regularMass = new RegularMass();
-        regularMass.setDay(TODAY.getDayOfWeek());
+        regularMass.setDay(DayOfWeek.MONDAY);
         config.setRegularMasses(List.of(regularMass));
         final var firstMass = new PlanningMassTemplate();
-        firstMass.setDateTime(LocalDateTime.of(TODAY, LocalTime.of(11,0)));
+        firstMass.setDateTime(LocalDateTime.of(LAST_MONDAY, LocalTime.of(11,0)));
         final var secondMass = new PlanningMassTemplate();
-        secondMass.setDateTime(LocalDateTime.of(TODAY.plusWeeks(1), LocalTime.of(11,0)));
+        secondMass.setDateTime(LocalDateTime.of(LAST_MONDAY.plusWeeks(1), LocalTime.of(11,0)));
         final var thirdMass = new PlanningMassTemplate();
-        thirdMass.setDateTime(LocalDateTime.of(TODAY.plusWeeks(2), LocalTime.of(11,0)));
+        thirdMass.setDateTime(LocalDateTime.of(LAST_MONDAY.plusWeeks(2), LocalTime.of(11,0)));
         final var lastSchedule = new Schedule(new ScheduleTemplate(List.of(thirdMass)), config);
         assertEquals(2, lastSchedule.getFutureDraftMasses().size());
-        assertEquals(TODAY.getDayOfWeek(), lastSchedule.getFutureDraftMasses().get(0).getDateTime().getDayOfWeek());
-        assertEquals(TODAY.getDayOfWeek(), lastSchedule.getFutureDraftMasses().get(1).getDateTime().getDayOfWeek());
+        assertEquals(LAST_MONDAY.getDayOfWeek(), lastSchedule.getFutureDraftMasses().get(0).getDateTime().getDayOfWeek());
+        assertEquals(LAST_MONDAY.getDayOfWeek(), lastSchedule.getFutureDraftMasses().get(1).getDateTime().getDayOfWeek());
         final var middleSchedule = new Schedule(new ScheduleTemplate(List.of(secondMass)), lastSchedule, config);
         assertEquals(1, middleSchedule.getFutureDraftMasses().size());
-        assertEquals(TODAY.getDayOfWeek(), middleSchedule.getFutureDraftMasses().get(0).getDateTime().getDayOfWeek());
+        assertEquals(LAST_MONDAY.getDayOfWeek(), middleSchedule.getFutureDraftMasses().get(0).getDateTime().getDayOfWeek());
         final var firstSchedule = new Schedule(new ScheduleTemplate(List.of(firstMass)), middleSchedule, config);
         assertEquals(0, firstSchedule.getFutureDraftMasses().size());
     }

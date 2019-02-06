00d7ed804cd9ed11d8f8cec79dd807675078ed39
Martin Kröning <m.kroening@hotmail.de>
2019-02-06 16:01:21 +0100

    Use try-with-resources in XSSF, use var instead of the implementation

--- core/src/main/java/org/altarplanner/core/xlsx/XSSF.java
+++ core/src/main/java/org/altarplanner/core/xlsx/XSSF.java
@@ -26,187 +26,189 @@ import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageAccess;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFDataFormat;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class XSSF {
 
   public static void exportScheduleCompact(Schedule schedule, Path output, int columns)
       throws IOException {
-    final XSSFWorkbook workbook = new XSSFWorkbook();
-    final XSSFSheet sheet = workbook.createSheet();
+    try (final var workbook = new XSSFWorkbook()) {
+      final var sheet = workbook.createSheet();
 
-    final XSSFCellStyle topOpenCellStyle = workbook.createCellStyle();
-    topOpenCellStyle.setBorderLeft(BorderStyle.THIN);
-    topOpenCellStyle.setBorderRight(BorderStyle.THIN);
-    topOpenCellStyle.setBorderBottom(BorderStyle.THIN);
+      final var topOpenCellStyle = workbook.createCellStyle();
+      topOpenCellStyle.setBorderLeft(BorderStyle.THIN);
+      topOpenCellStyle.setBorderRight(BorderStyle.THIN);
+      topOpenCellStyle.setBorderBottom(BorderStyle.THIN);
 
-    final XSSFCellStyle bottomOpenCellStyle = workbook.createCellStyle();
-    bottomOpenCellStyle.setBorderTop(BorderStyle.THIN);
-    bottomOpenCellStyle.setBorderLeft(BorderStyle.THIN);
-    bottomOpenCellStyle.setBorderRight(BorderStyle.THIN);
+      final var bottomOpenCellStyle = workbook.createCellStyle();
+      bottomOpenCellStyle.setBorderTop(BorderStyle.THIN);
+      bottomOpenCellStyle.setBorderLeft(BorderStyle.THIN);
+      bottomOpenCellStyle.setBorderRight(BorderStyle.THIN);
 
-    final XSSFCellStyle topBottomOpenCellStyle = workbook.createCellStyle();
-    topBottomOpenCellStyle.setBorderLeft(BorderStyle.THIN);
-    topBottomOpenCellStyle.setBorderRight(BorderStyle.THIN);
+      final var topBottomOpenCellStyle = workbook.createCellStyle();
+      topBottomOpenCellStyle.setBorderLeft(BorderStyle.THIN);
+      topBottomOpenCellStyle.setBorderRight(BorderStyle.THIN);
 
-    final CellStyle headerCellStyle = workbook.createCellStyle();
-    headerCellStyle.setAlignment(HorizontalAlignment.CENTER);
+      final var headerCellStyle = workbook.createCellStyle();
+      headerCellStyle.setAlignment(HorizontalAlignment.CENTER);
 
-    final XSSFCell headerCell = sheet.createRow(0).createCell(0);
-    headerCell.setCellValue(
-        "Altar Plan: "
-            + schedule
-                .getPlanningWindow()
-                .getStart()
-                .format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM))
-            + " - "
-            + schedule
-                .getPlanningWindow()
-                .getEndInclusive()
-                .format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
-    headerCell.setCellStyle(headerCellStyle);
-    sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 2 * (columns - 1)));
+      final var headerCell = sheet.createRow(0).createCell(0);
+      headerCell.setCellValue(
+          "Altar Plan: "
+              + schedule
+                  .getPlanningWindow()
+                  .getStart()
+                  .format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM))
+              + " - "
+              + schedule
+                  .getPlanningWindow()
+                  .getEndInclusive()
+                  .format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+      headerCell.setCellStyle(headerCellStyle);
+      sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, 2 * (columns - 1)));
 
-    final List<Integer> columnHeights =
-        IntStream.range(0, columns).map(value -> 1).boxed().collect(Collectors.toList());
+      final List<Integer> columnHeights =
+          IntStream.range(0, columns).map(value -> 1).boxed().collect(Collectors.toList());
 
-    schedule
-        .getFinalDraftMasses()
-        .forEach(
-            mass -> {
-              int rowIndex = Collections.min(columnHeights);
-              final int columnIndex = columnHeights.indexOf(rowIndex);
-              int rowsToCreate = rowIndex - sheet.getLastRowNum() + mass.getServices().size() + 2;
-              if (mass.getAnnotation() != null) {
-                ++rowsToCreate;
-              }
-              IntStream.range(0, rowsToCreate)
-                  .forEach(value -> sheet.createRow(sheet.getLastRowNum() + 1));
-              rowIndex++;
+      schedule
+          .getFinalDraftMasses()
+          .forEach(
+              mass -> {
+                int rowIndex = Collections.min(columnHeights);
+                final int columnIndex = columnHeights.indexOf(rowIndex);
+                int rowsToCreate = rowIndex - sheet.getLastRowNum() + mass.getServices().size() + 2;
+                if (mass.getAnnotation() != null) {
+                  ++rowsToCreate;
+                }
+                IntStream.range(0, rowsToCreate)
+                    .forEach(value -> sheet.createRow(sheet.getLastRowNum() + 1));
+                rowIndex++;
 
-              final XSSFCell dateTimeCell = sheet.getRow(rowIndex++).createCell(2 * columnIndex);
-              dateTimeCell.setCellValue(
-                  mass.getDateTime().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
-              dateTimeCell.setCellStyle(bottomOpenCellStyle);
+                final var dateTimeCell = sheet.getRow(rowIndex++).createCell(2 * columnIndex);
+                dateTimeCell.setCellValue(
+                    mass.getDateTime().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+                dateTimeCell.setCellStyle(bottomOpenCellStyle);
 
-              final XSSFCell churchFormCell = sheet.getRow(rowIndex++).createCell(2 * columnIndex);
-              churchFormCell.setCellValue(mass.getChurch() + " - " + mass.getForm());
-              churchFormCell.setCellStyle(topOpenCellStyle);
+                final var churchFormCell = sheet.getRow(rowIndex++).createCell(2 * columnIndex);
+                churchFormCell.setCellValue(mass.getChurch() + " - " + mass.getForm());
+                churchFormCell.setCellStyle(topOpenCellStyle);
 
-              if (mass.getAnnotation() != null) {
-                final XSSFCell annotationCell =
-                    sheet.getRow(rowIndex++).createCell(2 * columnIndex);
-                annotationCell.setCellValue(mass.getAnnotation());
-                annotationCell.setCellStyle(topOpenCellStyle);
-              }
+                if (mass.getAnnotation() != null) {
+                  final var annotationCell = sheet.getRow(rowIndex++).createCell(2 * columnIndex);
+                  annotationCell.setCellValue(mass.getAnnotation());
+                  annotationCell.setCellStyle(topOpenCellStyle);
+                }
 
-              for (final Service service : mass.getServices()) {
-                XSSFCell serviceCell = sheet.getRow(rowIndex++).createCell(2 * columnIndex);
-                serviceCell.setCellValue(service.getDesc());
-                serviceCell.setCellStyle(topBottomOpenCellStyle);
-              }
+                for (final Service service : mass.getServices()) {
+                  final var serviceCell = sheet.getRow(rowIndex++).createCell(2 * columnIndex);
+                  serviceCell.setCellValue(service.getDesc());
+                  serviceCell.setCellStyle(topBottomOpenCellStyle);
+                }
 
-              sheet.getRow(rowIndex - 1).getCell(2 * columnIndex).setCellStyle(topOpenCellStyle);
-              columnHeights.set(columnIndex, rowIndex);
-            });
+                sheet.getRow(rowIndex - 1).getCell(2 * columnIndex).setCellStyle(topOpenCellStyle);
+                columnHeights.set(columnIndex, rowIndex);
+              });
 
-    sheet.getPrintSetup().setFitWidth((short) 1);
-    sheet.getPrintSetup().setFitHeight((short) 0);
-    sheet.setFitToPage(true);
+      sheet.getPrintSetup().setFitWidth((short) 1);
+      sheet.getPrintSetup().setFitHeight((short) 0);
+      sheet.setFitToPage(true);
 
-    IntStream.range(0, columns).forEach(value -> sheet.autoSizeColumn(2 * value));
+      IntStream.range(0, columns).forEach(value -> sheet.autoSizeColumn(2 * value));
 
-    workbook.write(Files.newOutputStream(output));
-
-    workbook.close();
+      try (final var outputStream = Files.newOutputStream(output)) {
+        workbook.write(outputStream);
+      }
+    }
   }
 
   public static void exportScheduleOverview(Schedule schedule, Path output) throws IOException {
-    final XSSFWorkbook workbook = new XSSFWorkbook();
-    final XSSFSheet sheet = workbook.createSheet();
-    final XSSFDataFormat format = workbook.createDataFormat();
-    final XSSFCellStyle isoLocalDateTimeWithoutSecondsCellStyle = workbook.createCellStyle();
-    isoLocalDateTimeWithoutSecondsCellStyle.setDataFormat(format.getFormat("NN, YYYY-MM-DD HH:MM"));
+    try (final var workbook = new XSSFWorkbook()) {
+      final var sheet = workbook.createSheet();
+      final var format = workbook.createDataFormat();
+      final var isoLocalDateTimeWithoutSecondsCellStyle = workbook.createCellStyle();
+      isoLocalDateTimeWithoutSecondsCellStyle.setDataFormat(
+          format.getFormat("NN, YYYY-MM-DD HH:MM"));
 
-    final int rowOffset = 3;
-    final int columnOffset = 2;
-    final char firstColumnChar = 'A' + columnOffset;
+      final int rowOffset = 3;
+      final int columnOffset = 2;
+      final char firstColumnChar = 'A' + columnOffset;
 
-    final XSSFRow dateTimeRow = sheet.createRow(0);
-    final XSSFRow churchFormRow = sheet.createRow(1);
-    final XSSFRow annotationRow = sheet.createRow(2);
-    annotationRow.createCell(0).setCellValue(Config.RESOURCE_BUNDLE.getString("altarServer"));
-    annotationRow.createCell(1).setCellValue(Config.RESOURCE_BUNDLE.getString("assignmentCount"));
-    IntStream.range(0, schedule.getFinalDraftMasses().size())
-        .forEach(
-            i -> {
-              final var mass = schedule.getFinalDraftMasses().get(i);
-              final var date =
-                  Date.from(mass.getDateTime().atZone(ZoneId.systemDefault()).toInstant());
-              final var dateTimeCell = dateTimeRow.createCell(columnOffset + i);
-              dateTimeCell.setCellValue(date);
-              dateTimeCell.setCellStyle(isoLocalDateTimeWithoutSecondsCellStyle);
-              churchFormRow
-                  .createCell(columnOffset + i)
-                  .setCellValue(mass.getChurch() + " - " + mass.getForm());
-              annotationRow.createCell(columnOffset + i).setCellValue(mass.getAnnotation());
-            });
+      final var dateTimeRow = sheet.createRow(0);
+      final var churchFormRow = sheet.createRow(1);
+      final var annotationRow = sheet.createRow(2);
+      annotationRow.createCell(0).setCellValue(Config.RESOURCE_BUNDLE.getString("altarServer"));
+      annotationRow.createCell(1).setCellValue(Config.RESOURCE_BUNDLE.getString("assignmentCount"));
+      IntStream.range(0, schedule.getFinalDraftMasses().size())
+          .forEach(
+              i -> {
+                final var mass = schedule.getFinalDraftMasses().get(i);
+                final var date =
+                    Date.from(mass.getDateTime().atZone(ZoneId.systemDefault()).toInstant());
+                final var dateTimeCell = dateTimeRow.createCell(columnOffset + i);
+                dateTimeCell.setCellValue(date);
+                dateTimeCell.setCellStyle(isoLocalDateTimeWithoutSecondsCellStyle);
+                churchFormRow
+                    .createCell(columnOffset + i)
+                    .setCellValue(mass.getChurch() + " - " + mass.getForm());
+                annotationRow.createCell(columnOffset + i).setCellValue(mass.getAnnotation());
+              });
 
-    IntStream.range(0, schedule.getServers().size())
-        .forEach(
-            serverIndex -> {
-              final int rowIndex = rowOffset + serverIndex;
-              final Server server = schedule.getServers().get(serverIndex);
-              final XSSFRow serverRow = sheet.createRow(rowIndex);
-              serverRow.createCell(0).setCellValue(server.getDesc());
-              serverRow
-                  .createCell(1)
-                  .setCellFormula(
-                      "COUNTA(" + firstColumnChar + (rowIndex + 1) + ":AMJ" + (rowIndex + 1) + ")");
-              IntStream.range(0, schedule.getFinalDraftMasses().size())
-                  .forEach(
-                      massIndex ->
-                          schedule
-                              .getFinalDraftMasses()
-                              .get(massIndex)
-                              .getServices()
-                              .parallelStream()
-                              .filter(service -> server.equals(service.getServer()))
-                              .findAny()
-                              .ifPresent(
-                                  service ->
-                                      serverRow
-                                          .createCell(columnOffset + massIndex)
-                                          .setCellValue(service.getType().getXmlID())));
-            });
+      IntStream.range(0, schedule.getServers().size())
+          .forEach(
+              serverIndex -> {
+                final int rowIndex = rowOffset + serverIndex;
+                final Server server = schedule.getServers().get(serverIndex);
+                final var serverRow = sheet.createRow(rowIndex);
+                serverRow.createCell(0).setCellValue(server.getDesc());
+                serverRow
+                    .createCell(1)
+                    .setCellFormula(
+                        "COUNTA("
+                            + firstColumnChar
+                            + (rowIndex + 1)
+                            + ":AMJ"
+                            + (rowIndex + 1)
+                            + ")");
+                IntStream.range(0, schedule.getFinalDraftMasses().size())
+                    .forEach(
+                        massIndex ->
+                            schedule
+                                .getFinalDraftMasses()
+                                .get(massIndex)
+                                .getServices()
+                                .parallelStream()
+                                .filter(service -> server.equals(service.getServer()))
+                                .findAny()
+                                .ifPresent(
+                                    service ->
+                                        serverRow
+                                            .createCell(columnOffset + massIndex)
+                                            .setCellValue(service.getType().getXmlID())));
+              });
 
-    sheet.createFreezePane(columnOffset, rowOffset);
-    sheet.setAutoFilter(
-        new CellRangeAddress(
-            rowOffset - 1,
-            rowOffset - 1 + schedule.getServers().size() - 1,
-            0,
-            columnOffset + schedule.getFinalDraftMasses().size() - 1));
+      sheet.createFreezePane(columnOffset, rowOffset);
+      sheet.setAutoFilter(
+          new CellRangeAddress(
+              rowOffset - 1,
+              rowOffset - 1 + schedule.getServers().size() - 1,
+              0,
+              columnOffset + schedule.getFinalDraftMasses().size() - 1));
 
-    IntStream.range(0, columnOffset + schedule.getServers().size() - 1)
-        .forEach(sheet::autoSizeColumn);
+      IntStream.range(0, columnOffset + schedule.getServers().size() - 1)
+          .forEach(sheet::autoSizeColumn);
 
-    workbook.write(Files.newOutputStream(output));
-    workbook.close();
+      try (final var outputStream = Files.newOutputStream(output)) {
+        workbook.write(outputStream);
+      }
+    }
   }
 
   public static List<String> readHeader(final Path input) {
-    try (final XSSFWorkbook workbook =
+    try (final var workbook =
         new XSSFWorkbook(OPCPackage.open(input.toFile(), PackageAccess.READ))) {
-      final XSSFRow headerRow = workbook.getSheetAt(0).getRow(0);
+      final var headerRow = workbook.getSheetAt(0).getRow(0);
       Objects.requireNonNull(headerRow, "Header row at sheet index 0 must not be null.");
       final List<Cell> cells = new ArrayList<>(headerRow.getLastCellNum());
       headerRow.forEach(cells::add);
@@ -226,9 +228,9 @@ public class XSSF {
       final int forenameColumnIndex,
       final int yearColumnIndex,
       final Map<DayOfWeek, Integer> absentOnDayOfWeekColumnIndices) {
-    try (final XSSFWorkbook workbook =
+    try (final var workbook =
         new XSSFWorkbook(OPCPackage.open(input.toFile(), PackageAccess.READ))) {
-      final XSSFSheet sheet = workbook.getSheetAt(0);
+      final var sheet = workbook.getSheetAt(0);
       return IntStream.range(1, sheet.getLastRowNum())
           .mapToObj(sheet::getRow)
           .map(
